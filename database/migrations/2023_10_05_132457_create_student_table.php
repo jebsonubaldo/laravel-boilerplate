add797186153667baf36bdf3825f9c854e404e38
Jebson Ubaldo <ubaldojebson@gmail.com>
2023-10-06 13:54:34 +0800

    initial commit

---
+++ database/migrations/2023_10_05_132457_create_student_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id('studno');
+            $table->string('name');
+            $table->integer('grade_level');
+            $table->string('course');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student');
+    }
+};
